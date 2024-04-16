d936abf54a871af82e3cc4dc8bb0981e77fa55f0
Antonio Jerez <antonio.jerez@gmail.com>
2024-04-15 20:37:27 -0400

    Diseño Matenimiento de productos, Agregar, Editar.

--- views/modules/products.php
+++ views/modules/products.php
@@ -6,10 +6,7 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Punto de venta</h1>
-            <h2>Administrar Productos</h2>
-            <h2>Administración</h2>
-            <h2>Cambio 2</h2>
+            <h1>Administrar Productos</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
@@ -36,46 +33,68 @@
                 <thead>
                     <tr>
                         <th style="width:10px">#</th>
-                        <th>Nombre</th>
-                        <th>Codigo</th>
-                        <th>Foto</th>
+                        <th>Imagen</th>
+                        <th>Código</th>
+                        <th>Descripción</th>
+                        <th>Categoría</th>
+                        <th>Stock</th>
+                        <th>Costo Unit.</th>
+                        <th>Precio</th>
+                        <th>Agregado</th>
                         <th>Acciones</th>                       
                     </tr>
                 </thead>
                 <tbody>                        
                     <tr>
                         <td>1</td>
-                        <td>Usuario Administrador</td>
-                        <td>admin</td>
-                        <td><img src="views/img/users/default/anonymous.png" class="img-thumbnail" width="40px"></td>
+                        <td><img src="views/img/productos/aspirador-industrial.png" alt="" class="img-thumbnail" width="60px"></td>
+                        <td>1001</td>
+                        <td>Descripción para este producto</td>
+                        <td>Categoría de producto</td>
+                        <td>80</td>
+                        <td>1,000.00</td>
+                        <td>1,500.00</td>
+                        <td>2024-01-01 00:00:00</td>
                         <td>
                             <div class="btn-group">                            
-                                <button class="btn btn-warning"><i class="fa fa-pen"></i></button>
-                                <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+                                <button class="btn btn-warning" data-toggle="modal" data-target="#modalEditProducts"><i class="fa fa-pen"></i></button>
+                                <button class="btn btn-danger" data-toggle="modal" data-target="#modalDeleteProducts"><i class="fa fa-times"></i></button>
                             </div>  
                         </td>
                     </tr>
+
                     <tr>
                         <td>1</td>
-                        <td>Usuario Administrador</td>
-                        <td>admin</td>
-                        <td><img src="views/img/users/default/anonymous.png" class="img-thumbnail" width="40px"></td>
+                        <td><img src="views/img/productos/aspirador-industrial.png" alt="" class="img-thumbnail" width="60px"></td>
+                        <td>1001</td>
+                        <td>Descripción para este producto</td>
+                        <td>Categoría de producto</td>
+                        <td>80</td>
+                        <td>1,000.00</td>
+                        <td>1,500.00</td>
+                        <td>2024-01-01 00:00:00</td>
                         <td>
                             <div class="btn-group">                            
-                                <button class="btn btn-warning"><i class="fa fa-pen"></i></button>
-                                <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+                            <button class="btn btn-warning" data-toggle="modal" data-target="#modalEditProducts"><i class="fa fa-pen"></i></button>
+                            <button class="btn btn-danger" data-toggle="modal" data-target="#modalDeleteProducts"><i class="fa fa-times"></i></button>
                             </div>  
                         </td>
                     </tr>
+
                     <tr>
                         <td>1</td>
-                        <td>Usuario Administrador</td>
-                        <td>admin</td>
-                        <td><img src="views/img/users/default/anonymous.png" class="img-thumbnail" width="40px"></td>
+                        <td><img src="views/img/productos/aspirador-industrial.png" alt="" class="img-thumbnail" width="60px"></td>
+                        <td>1001</td>
+                        <td>Descripción para este producto</td>
+                        <td>Categoría de producto</td>
+                        <td>80</td>
+                        <td>1,000.00</td>
+                        <td>1,500.00</td>
+                        <td>2024-01-01 00:00:00</td>
                         <td>
                             <div class="btn-group">                            
-                                <button class="btn btn-warning"><i class="fa fa-pen"></i></button>
-                                <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+                            <button class="btn btn-warning" data-toggle="modal" data-target="#modalEditProducts"><i class="fa fa-pen"></i></button>
+                            <button class="btn btn-danger" data-toggle="modal" data-target="#modalDeleteProducts"><i class="fa fa-times"></i></button>
                             </div>  
                         </td>
                     </tr>
@@ -111,14 +130,61 @@
             </div>
 
             <div class="modal-body">
-                    <!-- Nombre de usuario-->
+                    <!--Codigo de producto-->
                     <div class="card-body">
-                      <div class="input-group mb-3">
-                        <div class="input-group-prepend">
-                          <span class="input-group-text"><i class="fa fa-user"></i></span>
+                        <div class="input-group mb-3">
+                          <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="fa fa-key"></i></span>
+                          </div>
+                          <input type="text" name="InsProductCode" class="form-control" placeholder="Código de Producto">
+                        </div>   
+                      <!--Nombre del producto-->
+                        <div class="input-group mb-3">
+                          <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="fa fa-box"></i></span>
+                          </div>
+                          <input type="text" name="InsProductName" class="form-control" placeholder="Nombre de Producto">
                         </div>
-                        <input type="text" name="UserName" class="form-control" placeholder="Nombre Usuario">
-                      </div>      
+                      <!--Categoría del producto-->
+                      <div class="input-group mb-3">
+
+                        <div class="input-group-prepend">
+                          <span class="input-group-text"><i class="fa fa-bars"></i></span>
+                        </div>
+
+                        <select class="form-control select2 select2-hidden-accessible" name ="InsProductCategory" style="width: 80%;" data-select2-id="1" tabindex="-1" aria-hidden="true">
+                          <option value = "" selected="selected" data-select2-id="0">Selecionar Categoría</option>
+                          <option value = "Administrador">Categoría 1</option>
+                          <option value = "Especial">Categoría 1</option>
+                          <option value = "Vendedor">Categoría 1</option>
+                        </select>
+
+                      </div> 
+
+                      <!--Existencia del producto-->
+                      <div class="input-group mb-3">
+                          <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="fa fa-socks"></i></span>
+                          </div>
+                          <input type="number" name="InsProductStock" class="form-control" placeholder="Existencia de Producto">
+                      </div>
+
+                      <!--Costo del producto-->
+                      <div class="input-group mb-3">
+                          <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="fa fa-folder-plus"></i></span>
+                          </div>
+                          <input type="number" min=0 name="InsProductCost" class="form-control" placeholder="Costo de Producto">
+                      </div>
+
+                      <!--Precio del producto-->
+                      <div class="input-group mb-3">
+                          <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="fa fa-folder-minus"></i></span>
+                          </div>
+                          <input type="number" min=0 name="InsProductPrice" class="form-control" placeholder="Precio de Producto">
+                      </div>
+
                       <!-- Foto -->
                       <div class="input-group mb-3">
                         <div class="input-group-prepend">
@@ -126,8 +192,8 @@
                         </div>
                         
                         <div class="custom-file">
-                          <input type="file" class="custom-file-input userpicture" id="exampleInputFile" name="picturefile">
-                          <label class="custom-file-label" for="exampleInputFile">Seleccione la Foto</label>
+                          <input type="file" class="custom-file-input userpicture" id="ProductImage" name="picturefile">
+                          <label class="custom-file-label" for="ProductImage">Seleccione la Foto del producto</label>
                         </div>
                         <div class="input-group-append">
                           <span class="input-group-text">Subir</span>
@@ -165,42 +231,60 @@
             </div>
 
             <div class="modal-body">
-                    <!-- Nombre de usuario-->
+                    <!--Codigo de producto-->
                     <div class="card-body">
-                      <div class="input-group mb-3">
-                        <div class="input-group-prepend">
-                          <span class="input-group-text"><i class="fa fa-user"></i></span>
+                        <div class="input-group mb-3">
+                          <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="fa fa-key"></i></span>
+                          </div>
+                          <input type="text" name="InsProductCode" class="form-control" placeholder="Código de Producto">
+                        </div>   
+                      <!--Nombre del producto-->
+                        <div class="input-group mb-3">
+                          <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="fa fa-box"></i></span>
+                          </div>
+                          <input type="text" name="InsProductName" class="form-control" placeholder="Nombre de Producto">
                         </div>
-                        <input type="text" id="edUserName" name="edUserName" class="form-control" value="">
-                      </div>     
-                      <!-- Id de usuario-->
+                      <!--Categoría del producto-->
                       <div class="input-group mb-3">
+
                         <div class="input-group-prepend">
-                          <span class="input-group-text"><i class="fa fa-key"></i></span>
-                        </div>
-                        <input type="text" id="edUserId" name="edUserId" class="form-control" value="" readonly>
-                      </div> 
-                      <!-- Contraseña-->
-                      <div class="input-group mb-3">
-                        <div class="input-group-prepend">
-                          <span class="input-group-text"><i class="fa fa-lock"></i></span>
-                        </div>
-                        <input type="text" name="UserPassword" class="form-control" value="">
-                        <input type="hidden" id="edUserPassword" name="edUserPassword" id="edUserPassword">
-                      </div> 
-                      <!-- Perfil -->
-                      <div class="input-group mb-3">
-                        <div class="input-group-prepend">
-                          <span class="input-group-text"><i class="fa fa-users"></i></span>
+                          <span class="input-group-text"><i class="fa fa-bars"></i></span>
                         </div>
 
-                        <select class="form-control select2 select2-hidden-accessible" id="edUserProfile" name ="edUserProfile" style="width: 80%;" data-select2-id="1" tabindex="-1" aria-hidden="true">
-                          <option value = ""  selected="selected" data-select2-id="0"></option>
-                          <option value = "Administrador">Administrador</option>
-                          <option value = "Especial">Especial</option>
-                          <option value = "Vendedor">Vendedor</option>
+                        <select class="form-control select2 select2-hidden-accessible" name ="InsProductCategory" style="width: 80%;" data-select2-id="1" tabindex="-1" aria-hidden="true">
+                          <option value = "" selected="selected" data-select2-id="0">Selecionar Categoría</option>
+                          <option value = "Administrador">Categoría 1</option>
+                          <option value = "Especial">Categoría 1</option>
+                          <option value = "Vendedor">Categoría 1</option>
                         </select>
+
                       </div> 
+
+                      <!--Existencia del producto-->
+                      <div class="input-group mb-3">
+                          <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="fa fa-socks"></i></span>
+                          </div>
+                          <input type="number" name="InsProductStock" class="form-control" placeholder="Existencia de Producto">
+                      </div>
+
+                      <!--Costo del producto-->
+                      <div class="input-group mb-3">
+                          <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="fa fa-folder-plus"></i></span>
+                          </div>
+                          <input type="number" min=0 name="InsProductCost" class="form-control" placeholder="Costo de Producto">
+                      </div>
+
+                      <!--Precio del producto-->
+                      <div class="input-group mb-3">
+                          <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="fa fa-folder-minus"></i></span>
+                          </div>
+                          <input type="number" min=0 name="InsProductPrice" class="form-control" placeholder="Precio de Producto">
+                      </div>
 
                       <!-- Foto -->
                       <div class="input-group mb-3">
@@ -209,8 +293,8 @@
                         </div>
                         
                         <div class="custom-file">
-                          <input type="file" class="custom-file-input userpicture" id="edpicturefile" name="edpicturefile">
-                          <label class="custom-file-label" for="exampleInputFile">Seleccione la Foto</label>
+                          <input type="file" class="custom-file-input userpicture" id="ProductImage" name="picturefile">
+                          <label class="custom-file-label" for="ProductImage">Seleccione la Foto del producto</label>
                         </div>
                         <div class="input-group-append">
                           <span class="input-group-text">Subir</span>
@@ -220,7 +304,6 @@
 
                       <div>
                         <img src="views/img/users/default/anonymous.png" alt="Foto" class="img-thumbnail img-preview" width="100px">
-                        <input type="hidden" id="edImage" name = "edImage">
                         <p class="help-block">Menor a 5 MB</p>
                       </div>               
 
@@ -228,7 +311,7 @@
 
               <div class="modal-footer-pers">
                 <button type="button" class="btn btn-danger" data-dismiss="modal">Cerrar</button>        
-                <button type="submit" class="btn btn-primary">Actualizar</button>
+                <button type="submit" class="btn btn-primary">Guardar</button>
               </div>
             </div>
         </form>
